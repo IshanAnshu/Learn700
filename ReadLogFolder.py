7fd3a30ca9c5beb3ffe42b99bbaee3b76f67d078
Learn700 <ishanmisra1111@gmail.com>
2021-09-03 10:59:17 +0530

    Rectified manual code change to adjust to random folder structure

--- ReadLogFolder.py
+++ ReadLogFolder.py
@@ -56,9 +56,10 @@ def main():
         i = i + 1
         print('(', i, ')', ' of ', totalNumofFiles, 'Working on file = ', file)
         result.append(readLogFileJSONWay(file))
-    countResultZipObj = zip(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
-                            result[8], result[9], result[10], result[11], result[12], result[13], result[14],
-                            result[15])
+    #countResultZipObj = zip(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
+                            #result[8], result[9], result[10], result[11], result[12], result[13], result[14],
+                            #result[15])
+    countResultZipObj = zip(*result)
     countResultList = list(countResultZipObj)
     printResult(countResultList)
 
